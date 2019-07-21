b43e4a0b84a3e2327f5f5a4b0b623dd594848621
RoujiaD <52130654+RoujiaD@users.noreply.github.com>
2019-07-21 15:29:50 +0100

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -15,7 +15,7 @@ T_max=np.zeros((5,1))
 for i in range(5):
     t = cartpole.main()
     T_max[i] = t
-T_max = np.mean(T_max)
+T_max = round(np.mean(T_max))
 
 scores_Agent_f = Agent_f.Agent(T_max)
 # Use Agent_a to determine ratio
